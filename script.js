e3e1f170f6f742e6460e5d0283eb48d117d9f00a
neyneyalldayday <drfrankenstein123@gmail.com>
2024-04-24 18:04:28 -0500

    new class

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-const names = ['Ryan', 
-'Ja', 
-'Joseph', 
-'Kristin', 
-'Gabriel', 
-'Liam',
- 'Kate', 
- 'Negash', 
- 'Nick', 
- 'Carly', 
- 'Grace',
-  'Sandeep', 
-  'Kevin', 
-  'Andres', 
-  'Nikky']; // Add more names as needed
+const names = [
+  "Ryan",
+  "James ",
+  "Whitney ",
+  "Valentina ",
+  "Cory",
+  "Matthew W",
+  "Ashley",
+  "Kendra",
+  "Michael",
+  "irina",
+  "Cody",
+  "Bezawit",
+  "Danny",
+  "Brad",
+  "Alexander",
+  "Brian",
+  "Elijah",
+  "Caliph",
+  "Michael Ny",
+  "Matt K",
+  "Hakim",
+  "Gabby",
+  "Caleb",
+]; // Add more names as needed
 
-  const randomStudent = document.getElementById("random-stud")
-  const randomButton = document.getElementById("stud-but")
+const randomStudent = document.getElementById("random-stud");
+const randomButton = document.getElementById("stud-but");
 function getRandomName() {
   const randomIndex = Math.floor(Math.random() * names.length);
   return names[randomIndex];
 }
 
-
-
-randomButton.addEventListener("click", function(event){
-    event.preventDefault()
-    const studName = document.createElement('h1')
-    studName.textContent = getRandomName()
-    randomStudent.appendChild(studName)
-    setTimeout(() => {
-     randomStudent.innerHTML = ''
-    }, 3000)
-})
-
+randomButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const studName = document.createElement("h1");
+  studName.textContent = getRandomName();
+  randomStudent.appendChild(studName);
+  setTimeout(() => {
+    randomStudent.innerHTML = "";
+  }, 3000);
+});
